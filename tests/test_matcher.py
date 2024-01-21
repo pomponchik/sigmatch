47e32a1358f71e866fef30c74fbe59547e59ed04
Evgeniy Blinov <zheni-b@yandex.ru>
2024-01-21 18:27:37 +0300

    tests

--- tests/test_matcher.py
+++ tests/test_matcher.py
@@ -31,7 +31,7 @@ def test_random_functions():
         pass
     def function_12(c=False, c2=False):
         pass
-    
+
     assert SignatureMatcher().match(function_1) == True
     assert SignatureMatcher('.').match(function_2) == True
     assert SignatureMatcher('**').match(function_3) == True
@@ -44,6 +44,55 @@ def test_random_functions():
     assert SignatureMatcher('.', '.', 'c', 'c2', '*', '**').match(function_10) == True
     assert SignatureMatcher('.', '.', '.', 'c', 'c2', '*', '**').match(function_11) == True
     assert SignatureMatcher('c', 'c2').match(function_12) == True
+
+    assert SignatureMatcher('.').match(lambda x: None) == True
+    assert SignatureMatcher('.', '.').match(lambda x, y: None) == True
+    assert SignatureMatcher('.', '*').match(lambda x, *y: None) == True
+    assert SignatureMatcher('.', '**').match(lambda x, **y: None) == True
+
+
+def test_random_async_functions():
+    """
+    Проверяем, что слепки сигнатур функций отрабатывают корректно.
+    """
+    async def function_1():
+        pass
+    async def function_2(arg):
+        pass
+    async def function_3(**kwargs):
+        pass
+    async def function_4(*args, **kwargs):
+        pass
+    async def function_5(a, b):
+        pass
+    async def function_6(a, b, c):
+        pass
+    async def function_7(a, b, c=False):
+        pass
+    async def function_8(a, b, c=False, *d):
+        pass
+    async def function_9(a, b, c=False, *d, **e):
+        pass
+    async def function_10(a, b, c=False, c2=False, *d, **e):
+        pass
+    async def function_11(a, b, b2, c=False, c2=False, *d, **e):
+        pass
+    async def function_12(c=False, c2=False):
+        pass
+
+    assert SignatureMatcher().match(function_1) == True
+    assert SignatureMatcher('.').match(function_2) == True
+    assert SignatureMatcher('**').match(function_3) == True
+    assert SignatureMatcher('*', '**').match(function_4) == True
+    assert SignatureMatcher('.', '.').match(function_5) == True
+    assert SignatureMatcher('.', '.', '.').match(function_6) == True
+    assert SignatureMatcher('.', '.', 'c').match(function_7) == True
+    assert SignatureMatcher('.', '.', 'c', '*').match(function_8) == True
+    assert SignatureMatcher('.', '.', 'c', '*', '**').match(function_9) == True
+    assert SignatureMatcher('.', '.', 'c', 'c2', '*', '**').match(function_10) == True
+    assert SignatureMatcher('.', '.', '.', 'c', 'c2', '*', '**').match(function_11) == True
+    assert SignatureMatcher('c', 'c2').match(function_12) == True
+
 
 def test_random_wrong_functions():
     """
@@ -73,6 +122,7 @@ def test_random_wrong_functions():
         pass
     def function_12(c=False, c2=False):
         pass
+
     assert SignatureMatcher('.').match(function_1) == False
     assert SignatureMatcher('c').match(function_2) == False
     assert SignatureMatcher('.', '**').match(function_3) == False
@@ -85,3 +135,51 @@ def test_random_wrong_functions():
     assert SignatureMatcher('.', '.', 'c2', '*', '**').match(function_10) == False
     assert SignatureMatcher('.', '.', 'c2', '*', '**').match(function_11) == False
     assert SignatureMatcher('c').match(function_12) == False
+
+
+def test_random_wrong_async_functions():
+    """
+    Проверяем, что слепки сигнатур функций с неподходящими функциями не матчатся.
+    """
+    async def function_1():
+        pass
+    async def function_2(arg):
+        pass
+    async def function_3(**kwargs):
+        pass
+    async def function_4(*args, **kwargs):
+        pass
+    async def function_5(a, b):
+        pass
+    async def function_6(a, b, c):
+        pass
+    async def function_7(a, b, c=False):
+        pass
+    async def function_8(a, b, c=False, *d):
+        pass
+    async def function_9(a, b, c=False, *d, **e):
+        pass
+    async def function_10(a, b, c=False, c2=False, *d, **e):
+        pass
+    async def function_11(a, b, b2, c=False, c2=False, *d, **e):
+        pass
+    async def function_12(c=False, c2=False):
+        pass
+
+    assert SignatureMatcher('.').match(function_1) == False
+    assert SignatureMatcher('c').match(function_2) == False
+    assert SignatureMatcher('.', '**').match(function_3) == False
+    assert SignatureMatcher('.', '**').match(function_4) == False
+    assert SignatureMatcher('.', 'c').match(function_5) == False
+    assert SignatureMatcher('.', '.').match(function_6) == False
+    assert SignatureMatcher('.', '.').match(function_7) == False
+    assert SignatureMatcher('.', '.', 'c').match(function_8) == False
+    assert SignatureMatcher('.', 'c', '*', '**').match(function_9) == False
+    assert SignatureMatcher('.', '.', 'c2', '*', '**').match(function_10) == False
+    assert SignatureMatcher('.', '.', 'c2', '*', '**').match(function_11) == False
+    assert SignatureMatcher('c').match(function_12) == False
+
+    assert SignatureMatcher().match(lambda x: None) == False
+    assert SignatureMatcher('.').match(lambda x, y: None) == False
+    assert SignatureMatcher('*').match(lambda x, *y: None) == False
+    assert SignatureMatcher('**').match(lambda x, **y: None) == False
