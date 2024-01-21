739b16bb955b779b6b295425d49f530db2263958
Evgeniy Blinov <zheni-b@yandex.ru>
2024-01-21 22:29:24 +0300

    tests

--- tests/test_matcher.py
+++ tests/test_matcher.py
@@ -183,3 +183,89 @@ def test_random_wrong_async_functions():
     assert SignatureMatcher('.').match(lambda x, y: None) == False
     assert SignatureMatcher('*').match(lambda x, *y: None) == False
     assert SignatureMatcher('**').match(lambda x, **y: None) == False
+
+
+def test_random_wrong_generator_functions():
+    """
+    Проверяем, что слепки сигнатур функций с неподходящими функциями не матчатся.
+    """
+    def function_1():
+        yield None
+    def function_2(arg):
+        yield None
+    def function_3(**kwargs):
+        yield None
+    def function_4(*args, **kwargs):
+        yield None
+    def function_5(a, b):
+        yield None
+    def function_6(a, b, c):
+        yield None
+    def function_7(a, b, c=False):
+        yield None
+    def function_8(a, b, c=False, *d):
+        yield None
+    def function_9(a, b, c=False, *d, **e):
+        yield None
+    def function_10(a, b, c=False, c2=False, *d, **e):
+        yield None
+    def function_11(a, b, b2, c=False, c2=False, *d, **e):
+        yield None
+    def function_12(c=False, c2=False):
+        yield None
+
+    assert SignatureMatcher('.').match(function_1) == False
+    assert SignatureMatcher('c').match(function_2) == False
+    assert SignatureMatcher('.', '**').match(function_3) == False
+    assert SignatureMatcher('.', '**').match(function_4) == False
+    assert SignatureMatcher('.', 'c').match(function_5) == False
+    assert SignatureMatcher('.', '.').match(function_6) == False
+    assert SignatureMatcher('.', '.').match(function_7) == False
+    assert SignatureMatcher('.', '.', 'c').match(function_8) == False
+    assert SignatureMatcher('.', 'c', '*', '**').match(function_9) == False
+    assert SignatureMatcher('.', '.', 'c2', '*', '**').match(function_10) == False
+    assert SignatureMatcher('.', '.', 'c2', '*', '**').match(function_11) == False
+    assert SignatureMatcher('c').match(function_12) == False
+
+
+def test_random_generator_functions():
+    """
+    Проверяем, что слепки сигнатур функций отрабатывают корректно.
+    """
+    def function_1():
+        yield None
+    def function_2(arg):
+        yield None
+    def function_3(**kwargs):
+        yield None
+    def function_4(*args, **kwargs):
+        yield None
+    def function_5(a, b):
+        yield None
+    def function_6(a, b, c):
+        yield None
+    def function_7(a, b, c=False):
+        yield None
+    def function_8(a, b, c=False, *d):
+        yield None
+    def function_9(a, b, c=False, *d, **e):
+        yield None
+    def function_10(a, b, c=False, c2=False, *d, **e):
+        yield None
+    def function_11(a, b, b2, c=False, c2=False, *d, **e):
+        yield None
+    def function_12(c=False, c2=False):
+        yield None
+
+    assert SignatureMatcher().match(function_1) == True
+    assert SignatureMatcher('.').match(function_2) == True
+    assert SignatureMatcher('**').match(function_3) == True
+    assert SignatureMatcher('*', '**').match(function_4) == True
+    assert SignatureMatcher('.', '.').match(function_5) == True
+    assert SignatureMatcher('.', '.', '.').match(function_6) == True
+    assert SignatureMatcher('.', '.', 'c').match(function_7) == True
+    assert SignatureMatcher('.', '.', 'c', '*').match(function_8) == True
+    assert SignatureMatcher('.', '.', 'c', '*', '**').match(function_9) == True
+    assert SignatureMatcher('.', '.', 'c', 'c2', '*', '**').match(function_10) == True
+    assert SignatureMatcher('.', '.', '.', 'c', 'c2', '*', '**').match(function_11) == True
+    assert SignatureMatcher('c', 'c2').match(function_12) == True
